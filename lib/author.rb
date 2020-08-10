b0cfb25cd4610ea52931d052afa154d011f25318
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-10 00:32:04 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,12 +6,18 @@ class Author
     @name = name
   end
   
-  def add_post(pst)
-    pst.author = self
+  def add_post(post1)
+    post1.author = self
   end
   
   def posts
-    Post.all.select {|pst| pst.author == self}
+    Post.all.select {|post1| post1.author == self}
   end
+  
+  def add_post_by_title(title)
+    
+  end
+  
+  def self.post_count
   
 end
